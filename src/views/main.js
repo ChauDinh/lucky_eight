17b40f2fd0586a500f166775226d180c7b993c09
ChauDinh <chaudvb95uc@gmail.com>
2019-01-11 19:10:32 +0700

    add algorithm

--- src/views/main.js
+++ src/views/main.js
@@ -2,3 +2,15 @@ const randomNum = () => {
  return Math.floor(Math.random() * 10);
 }
 
+const buttonElement = document.getElementById("button");
+const numberElement = document.getElementById("number");
+const winnerElement = document.getElementById("winner");
+
+buttonElement.addEventListener("click", e => {
+ const random = randomNum();
+ number.textContent = random;
+
+ random === 8 ? winnerElement.style.display = "initial" : winnerElement.style.display = "none";
+});
+
+
